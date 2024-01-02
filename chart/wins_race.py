e36ec3a5fbd136441c102b242738682714b650e4
Ryan Day <ryandaydev@gmail.com>
2024-01-02 02:30:53 +0000

    Change color of race plot

--- chart/wins_race.py
+++ chart/wins_race.py
@@ -97,7 +97,7 @@ class WinsRace():
             width=chart_width,
             height=chart_height,
             #paper_bgcolor="lightgray",
-            paper_bgcolor="Black",
+            paper_bgcolor="blue",
             margin_t=25 #top margin of chart -         #https://plotly.com/python/reference/#layout-margin
             )
             st.plotly_chart(fig, use_container_width=True)
